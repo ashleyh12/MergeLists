ff6166dd1e8dd35665db29ac82be0945624afe8f
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-01-04 15:02:59 -0500

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,12 @@
+def Mergetwolists(list1, list2):
+  empty_list = []
+  length = len(list1)
+  length1 = len(list2)
+  for i in range(length):
+    empty_list.append(list1[i])
+  for i in range(length1):
+    empty_list.append(list2[i])
+  empty_list.sort()
+  return empty_list
+
+# print(Mergetwolists([1, 2, 4], [1, 3, 4])) .... example input
